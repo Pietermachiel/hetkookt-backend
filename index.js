b43c0a3351059c369814e0e0b8120416b0f35028
Pietermachiel <pieter@roozen.nl>
2020-04-11 17:50:56 +0200

    c

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 mongoose
-  .connect(
-    "mongodb+srv://Pietermachiel:pieter19machiel54lambert@cluster0-npjfk.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect("mongodb://127.0.0.1/mongoose", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Connected to MongoDB..."));
 
 app.listen(
