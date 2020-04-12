0df748c87a0a31775ac6d0170b5ee0ff9c4589b9
Pietermachiel <pieter@roozen.nl>
2020-04-12 17:56:47 +0200

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -4,11 +4,8 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
-  const user = config.get("user");
-  const password = config.get("password");
-  const dbName = config.get("dbName");
   mongoose
-    .connect(process.env.DB || db, {
+    .connect(db, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true,
