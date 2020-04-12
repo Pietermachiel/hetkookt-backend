4dfac80e695a9094d11e3ffa4744c59abd9e7a17
Pietermachiel <pieter@roozen.nl>
2020-04-12 17:22:03 +0200

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -4,12 +4,18 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
+  const user = config.get("user");
+  const password = config.get("password");
+  const dbName = config.get("dbName");
   mongoose
     .connect(db, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true,
       useUnifiedTopology: true,
+      user: user,
+      password: password,
+      dbName: dbName,
     })
     .then(() => console.log("Connected to MongoDB..."));
   // .then(() => winston.info(`Connected to ${db}...`));
