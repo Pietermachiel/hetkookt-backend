c74e2d0b6eac3648cfa75d98b90af7b1ce8400dd
Pietermachiel <pieter@roozen.nl>
2020-04-12 18:30:35 +0200

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -5,12 +5,15 @@ const config = require("config");
 module.exports = function () {
   const db = config.get("db");
   mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://Pietermachiel:pieter19machiel54lambert@cluster0-npjfk.mongodb.net/favorites?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("Connected to MongoDB..."));
   // .then(() => winston.info(`Connected to ${db}...`));
 };
