249dc9a4e997d0b513ee6bc3395e7ff98efb1a65
Pietermachiel <pieter@roozen.nl>
2020-07-28 18:02:35 +0200

    Update routes.js

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const users = require("../routes/users");
-const books = require("../routes/books");
-const recipes = require("../routes/recipes");
-const kitchens = require("../routes/kitchens");
-const tags = require("../routes/tags");
-const categories = require("../routes/categories");
+// const recipes = require("../routes/recipes");
+// const books = require("../routes/books");
+// const kitchens = require("../routes/kitchens");
+// const tags = require("../routes/tags");
+// const categories = require("../routes/categories");
 const auth = require("../routes/auth");
 // const error = require("../middleware/error");
 
@@ -12,10 +12,10 @@ module.exports = function (app) {
   app.use(express.json());
   app.use("/api/auth", auth);
   app.use("/api/users", users);
-  app.use("/api/recipes", recipes);
-  app.use("/api/books", books);
-  app.use("/api/kitchens", kitchens);
-  app.use("/api/tags", tags);
-  app.use("/api/categories", categories);
+  // app.use("/api/recipes", recipes);
+  // app.use("/api/books", books);
+  // app.use("/api/kitchens", kitchens);
+  // app.use("/api/tags", tags);
+  // app.use("/api/categories", categories);
   // app.use(error);
 };
