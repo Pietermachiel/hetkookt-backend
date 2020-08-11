32debfbfec22ed1104fc8219d3a1ddce33309e6a
Pietermachiel <pieter@roozen.nl>
2020-08-11 09:17:43 +0200

    Update routes.js

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const users = require("../routes/users");
 const recipes = require("../routes/recipes");
-// const books = require("../routes/books");
-// const kitchens = require("../routes/kitchens");
+const books = require("../routes/books");
+const kitchens = require("../routes/kitchens");
 // const tags = require("../routes/tags");
 // const categories = require("../routes/categories");
 const auth = require("../routes/auth");
@@ -13,8 +13,8 @@ module.exports = function (app) {
   app.use("/api/auth", auth);
   app.use("/api/users", users);
   app.use("/api/recipes", recipes);
-  // app.use("/api/books", books);
-  // app.use("/api/kitchens", kitchens);
+  app.use("/api/books", books);
+  app.use("/api/kitchens", kitchens);
   // app.use("/api/tags", tags);
   // app.use("/api/categories", categories);
   // app.use(error);
