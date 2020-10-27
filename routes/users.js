25f47168d05bd14d53d9b3c09585b088f017787f
Pietermachiel <pieter@roozen.nl>
2020-10-27 14:09:55 +0100

    sendgrid

--- routes/users.js
+++ routes/users.js
@@ -12,61 +12,12 @@ const { validateRecipe } = require("../models/recipe");
 var nodemailer = require("nodemailer");
 var sgTransport = require("nodemailer-sendgrid-transport");
 
-// function asyncMiddleware(handler) {
-//   return async (req, res, next) => {
-//     try {
-//       await handler(req, res);
-//     } catch (ex) {
-//       next(ex);
-//     }
-//   };
-// }
-
 router.get("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id)
-    // .populate({
-    //   path: "items",
-    //   populate: {
-    //     path: "tags",
-    //     populate: { path: "category" },
-    //   },
-    //   populate: "related",
-    // })
-    // .populate("related")
-    .select("-password -__v");
-  // console.log(user.items);
+  const user = await User.findById(req.user._id).select("-password -__v");
   res.send(user);
 });
 
-// app.use(error)
-
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const users = await User.find();
-//     // .populate("recipes", "favorites", "stock", "title author active")
-//     // .select("-__v -password -email")
-//     // .sort("name");
-//     res.send(users);
-//   } catch (ex) {
-//     next(ex);
-//     // res.status(500).send("something failed");
-//   }
-// });
-
-// app.use(async)
-
-// router.get(
-//   "/",
-//   asyncMiddleware(async (req, res, next) => {
-//     const users = await User.find();
-//     res.send(users);
-//   })
-// );
-
-// require("express-async-errors");
-
 router.get("/", async (req, res) => {
-  // throw new Error("could not get");
   const users = await User.find();
   res.send(users);
 });
@@ -97,7 +48,6 @@ router.post("/", async (req, res) => {
 
   var options = {
     auth: {
-      api_user: process.env.EMAIL_USER,
       api_key: process.env.EMAIL_PASSWORD,
     },
   };
@@ -110,36 +60,16 @@ router.post("/", async (req, res) => {
     subject: "Welkom bij hetKookt",
     text: "Welkom bij hetKookt",
     html: mailtemplate(user.name, user.temporarytoken),
-
-    // html: `<h1 style="color: red;">hetkookt</h1><br>
-    // <p style="font-weight: normal">Welkom<strong> ${user.name}</strong>,<br><br>Bevestig je inschrijving door op onderstaande link te klikken: </p>
-    // <button style="
-    // background-color: #407afc;
-    // color: #fff;
-    // text-transform: uppercase;
-    // text-align: center;
-    // font-weight: 500;
-    // padding: 1.5rem 2rem;
-    // display: inline-block;
-    // border-radius: 2px;
-    // border: 1px solid transparent;
-    // letter-spacing: .1em;
-    // font-size: .75rem;
-    // line-height: 1;">
-    // <a style="color: white; text-decoration: none;" href="http://localhost:3000/verify/${user.temporarytoken}">Bevestig mijn inschrijving</a>
-    // </button>`,
-    // templateId: "d-96cc73c046b244f8b7c15f3938b4906b",
-    // dynamic_template_data: {
-    //   name: user.name,
-    // },
   };
 
   // console.log(client);
 
   client.sendMail(email, function (err, info) {
     if (err) {
+      // console.log("error");
       console.log(error);
     } else {
+      // console.log("Message sent");
       console.log("Message sent: " + info.response);
     }
   });
@@ -147,8 +77,9 @@ router.post("/", async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
+  // console.log("user");
   // console.log(user);
-  // res.send(user);
+  // console.log(user);  // res.send(user);
   const token = user.generateAuthToken(); // = method user model
   // console.log(token);
 
@@ -256,33 +187,3 @@ router.delete("/:id", async (req, res) => {
 });
 
 module.exports = router;
-
-// // recipes
-
-// router.get("/recipes", auth, async (req, res) => {
-//   const user = await User.findById(req.user._id)
-//     .select("-__v")
-//     .sort("title")
-//     .populate("dish")
-//     .populate("tags")
-//     .populate("book");
-//   const recipes = user.recipes;
-//   res.send(recipes);
-// });
-
-// router.put("/recipes/:id", async (req, res) => {
-//   // const { error } = validateRecipe(req.body.recipes);
-//   // if (error) return res.status(400).send(error.details[0].message);
-
-//   const user = await User.findByIdAndUpdate(
-//     req.params.id,
-//     {
-//       recipes: req.body.recipes,
-//     },
-//     {
-//       new: true,
-//     }
-//   );
-//   // console.log("plus");
-//   res.send(user);
-// });
